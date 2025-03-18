7f7da2aa0c380c47036582f87dbcb55c7a299636
Pedro Ap <pedroaparecidori@gmail.com>
2025-03-18 14:14:58 -0300

    ultimo commit do ntalk

--- ex/text_async.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-for (let i = 1; i <= 5; i++) {
-    const file = `async-txt${i}.txt`
-    fs.writeFile(file, 'Hello Node.js!', (err, out) => {
-        console.log(out)
-    })
-}
